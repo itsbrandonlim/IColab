9ba08135165cf2163d60e4ad1bcc4a032023b65d
Kevin Dallian <kevindallian@gmail.com>
2023-12-30 17:35:15 +0700

    changed logic to store project inside account detail using ids

---
+++ IColab/Features/Data/UseCases/FireStore/FetchProjectsFromOwnerID.swift
@@ -0,0 +1,18 @@
+//
+//  FetchProjectsFromOwnerID.swift
+//  IColab
+//
+//  Created by Kevin Dallian on 30/12/23.
+//
+
+import SwiftUI
+
+struct FetchProjectsFromOwnerID: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FetchProjectsFromOwnerID()
+}
