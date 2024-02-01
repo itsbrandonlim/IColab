3802f1a63af97a51ed06f80422bbb7ec2d362d36
JeremyR <metekohy233@gmail.com>
2024-02-01 12:24:05 +0700

    Some small changes and added isAdmin property to user

---
+++ IColab/Features/Presentation/Project/Components/PaymentView/Owner/ViewModels/OwnerPaymentManagementViewModels.swift
@@ -0,0 +1,12 @@
+//
+//  OwnerPaymentViewModels.swift
+//  IColab
+//
+//  Created by Jeremy Raymond on 23/01/24.
+//
+
+import Foundation
+
+class OwnerPaymentViewModels: ObservableObject {
+    
+}
