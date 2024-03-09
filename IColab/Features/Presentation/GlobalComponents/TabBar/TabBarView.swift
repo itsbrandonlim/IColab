673904c0a889e87fb99993ebabfc612b66f5cb3f
Kevin Dallian <kevindallian@gmail.com>
2024-03-09 15:21:24 +0700

    deleted duplicate references and resolve conflicts

--- IColab/Features/Presentation/GlobalComponents/TabBar/TabBarView.swift
+++ IColab/Features/Presentation/GlobalComponents/TabBar/TabBarView.swift
@@ -39,13 +39,8 @@ struct TabBarView: View {
 struct TabBarView_Previews: PreviewProvider {
     static var previews: some View {
         VStack{
-<<<<<<< HEAD
-            HomeView()
-            TabBarView(selectedTabItem: .constant(.home), tabBarItems: [.home])
-=======
             BrowseView()
-            TabBarView(selectedTabItem: .constant(.home))
->>>>>>> Development
+            TabBarView(selectedTabItem: .constant(.home), tabBarItems: [.home])
         }
         
     }
